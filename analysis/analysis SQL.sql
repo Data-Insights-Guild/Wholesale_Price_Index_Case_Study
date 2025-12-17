0d75b21b4c01fccde2aa16597180dfd3e96f6418
manya <manyashreedeo@gmail.com>
2025-12-17 15:27:24 +0530

    analysis on sql done

---
+++ analysis/analysis SQL.sql
@@ -0,0 +1,9 @@
+SELECT * FROM wpi_comm;
+
+SELECT * FROM wpi_comm_info;
+SELECT COUNT(commodity) FROM wpi_comm_info;
+DROP TABLE wpi_comm;
+SELECT * FROM wpi_energy;
+SELECT * FROM wpi_food;
+SELECT * FROM wpi_only;
+SELECT * FROM wpi_comm;
